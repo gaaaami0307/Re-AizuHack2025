012cca8a399eabbbca80427d7502aac94a4819d5
がみ <s1320016@u-aizu.ac.jp>
2025-06-16 06:04:22 +0000

    complete:setting

--- start.sh
+++ start.sh
@@ -2,22 +2,5 @@
 
 set -e  # エラーがあったらそこで止まる
 
-echo "依存関係インストール"
-sudo apt update
-sudo apt install ruby-full
-sudo apt install mysql-server
-sudo gem install bundler
-sudo apt-get update
-sudo apt-get install -y default-libmysqlclient-dev build-essential
-sudo apt-get install -y libyaml-dev
-
-
-sudo bundle install
-#yarn install
-
-echo "データベース作成"
-sudo service mysql start
-rails db:create
-rails db:migrate
-
-echo "完了"
+echo "Railsサーバー起動"
+rails server -b 0.0.0.0 -p 3000
